556b9f8aca7256bdb81dd59dceaf33e9ce1608f7
jonathanrundjan <jonathan.yitskhaq@ui.ac.id>
2025-11-19 16:07:28 +0700

    add test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,6 +11,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:football_news/main.dart';
 
 void main() {
+  test('dummy test – always passes', () {
+    expect(1, 1);
+  });
+
   // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
   //   // Build our app and trigger a frame.
   //   await tester.pumpWidget(const MyApp());
